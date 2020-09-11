e9d7eaa3116384cc432c4f805557ca5da381b7c5
Hemalatha.R <hemaangeleen2730@gmail.com>
2020-09-11 10:41:29 +0530

    ques1.js

---
+++ Day-3-4/scripts/ques1.js
@@ -0,0 +1,14 @@
+function btn_1() {
+    document.getElementById("image").src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.housebeautiful.com%2Flifestyle%2Fgardening%2Fg13074130%2Fbeautiful-flower-images%2F&psig=AOvVaw2UFSoDgjqaTy_eZgo4DjYZ&ust=1599886709335000&source=images&cd=vfe&ved=2ahUKEwjAlJnJqODrAhUUC7cAHSIWBVcQr4kDegUIARDSAQ";
+}
+
+function btn_2() {
+    const img2 = "https://i.pinimg.com/originals/11/2b/74/112b746a2182417b2a947d949798c968.jpg";
+    document.getElementById("image").src = img3;
+}
+
+
+function btn_3() {
+    const img3 = "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg";
+    document.getElementById("image").src = img3;
+}
