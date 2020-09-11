47362b22fa046a1f00b10fc1523f39d85270c15e
Hemalatha.R <hemaangeleen2730@gmail.com>
2020-09-11 10:56:15 +0530

    ques3.js

---
+++ Day-3-4/scripts/ques3.js
@@ -0,0 +1,32 @@
+let Friends = [{
+        name: "pooja",
+        age: 20,
+        country: "India",
+        hobbies: ['draw', 'dance', 'cook']
+    },
+    {
+        name: "viji",
+        age: 27,
+        country: "Moscow",
+        hobbies: ['play', 'coin collection', 'hack']
+    },
+    {
+        name: "Sam",
+        age: 25,
+        country: "New york",
+        hobbies: ['sing', 'write', 'play']
+    },
+    {
+        name: "Guna",
+        age: 22,
+        country: "New jersey",
+        hobbies: ['gardening', 'guitar playing', 'cook']
+    }
+];
+
+function display(Friends) {
+
+    console.log(Friends);
+}
+
+display(Friends);
